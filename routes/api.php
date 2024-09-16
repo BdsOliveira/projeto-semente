49f54f52a7244f3058d378ed06b990d7090a6a2e
Bruno Oliveira <bds.commus@gmail.com>
2024-09-16 09:15:10 -0300

    feat: add pessoas routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('pessoas/', function () {
+    return 'index pessoas';
+});
+Route::get('pessoas/{id}', function ($id) {
+    return "pessoa {$id}";
+});
+Route::post('pessoas/', function () {
+    return 'post pessoas';
+});
+Route::put('pessoas/{id}', function ($id) {
+    return "put pessoa {$id}";
+});
+Route::delete('pessoas/{id}', function ($id) {
+    return "delete pessoa {$id}";
+});
