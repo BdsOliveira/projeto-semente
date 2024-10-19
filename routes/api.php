f5b7daf4aa8d5107b6a332c6157f1fb6591fdd71
Bruno Oliveira <bds.commus@gmail.com>
2024-10-19 08:55:11 -0300

    finaliza api.php

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('pessoas/', function () {
     return 'index pessoas';
 });
-Route::get('pessoas/{id}', function ($id) {
-    return "pessoa {$id}";
+Route::get('pessoas/{id}', function (Request $request, $id) {
+    return $request->get('title');
 });
 Route::post('pessoas/', function () {
     return 'post pessoas';
